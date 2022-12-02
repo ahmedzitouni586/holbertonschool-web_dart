6593c34ddfa2b76fb6422dd02eda1dc0fc51a7aa
Ahmed <zitouniahmed595@gmail.com>
2022-12-02 20:57:09 +0100

    password

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,7 +1,7 @@
 class Password {
   String password = "";
   bool isValid() {
-    if (password.length >= 8 && password.length <= 16 && password.contains(r'^[a-zA-Z0-9]*$')) {
+    if (password.length >= 8 && password.length <= 1 && password.contains(RegExp(r'^[a-zA-Z0-9]*$'))) {
       return true;
     }
     return false;
