cfe754a0fe47c9ec6dc55509ddaabf9fe479399b
Ahmed <zitouniahmed595@gmail.com>
2022-12-02 21:10:05 +0100

    private

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -4,8 +4,10 @@ class Password {
     this._password = password;
   }
   bool isValid() {
-    if (this._password.length > 6 && this._password.length <18 && this._password.contains(RegExp(r'^[a-zA-Z\d]*$'))) {
-      return true;
+    if (this._password.length > 6 && this._password.length <18 && this._password.contains(RegExp(r'^[a-zA-Z0]*$'))) {
+      if (this._password.contains(RegExp(r'\d'))) {
+          return true;
+        }
     }
     return false;
   }
@@ -13,4 +15,13 @@ class Password {
   String toString() {
     return "Your Password is: ${this._password}";
   }
+}
+
+void main() {
+  final ps = Password(password: "Passwordcode");
+  print(ps.toString());
+  print(ps.isValid());
+  final ps2 = Password(password: "PasswordDecode3");
+  print(ps2.toString());
+  print(ps2.isValid());
 }
